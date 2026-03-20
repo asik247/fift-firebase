a928a803406b14eccf91915c64f4cf8cbf5ac572
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-20 22:06:54 +0600

    Nav,and other components setup

---
+++ src/Components/Registation/Registation.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Registation = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col ">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold">Registaion now!</h1>
+
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <form>
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <button className="btn btn-neutral mt-4">Registaion</button>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Registation;
