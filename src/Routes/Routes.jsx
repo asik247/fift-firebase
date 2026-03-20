ddf360b3db92bce731f01d8faf5b6a520a84a6b5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-20 22:11:50 +0600

    routers setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Components/HomePage/Home";
+import Registation from "../Components/Registation/Registation";
+import LogIn from "../Components/LogIn/LogIn";
+import Orders from "../Components/Orders/Orders";
+import Profile from "../Components/Profile/Profile";
+import DashBoard from "../Components/DashBoard/DashBoard";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            { index: true, Component: Home },
+            { path: 'registation', Component: Registation },
+            { path: 'login', Component: LogIn },
+            { path: 'orders', Component: Orders },
+            { path: 'profile', Component: Profile },
+            { path: 'dashBoard', Component: DashBoard },
+        ]
+
+    }
+])
+export default router
